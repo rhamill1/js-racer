12a27d242b5b38b910fcfb9457f61a896b8e0436
rhamill1 <rhamill11@gmail.com>
2016-09-09 23:04:50 -0700

    party take 1

--- code/app.js
+++ code/app.js
@@ -9,64 +9,77 @@ window.onload = function() {
     var redBoxPosi1 = 0;
     var blueBoxPosi1 = 0;
 
-
-
     function moveBox(e) {
         switch(e.keyCode) {
             case 39:
                 redBoxPosi1+=10;
                 redBox.style.left=redBoxPosi1+"px";
+                // console.log(redBoxPosi1);
+                declareWin(redBoxPosi1, blueBoxPosi1);
                 break;
             case 68:
-                blueBoxPosi1+=10;
+                blueBoxPosi1+=11;
                 blueBox.style.left=blueBoxPosi1+"px";
+                declareWin(redBoxPosi1, blueBoxPosi1);
+                // console.log(blueBoxPosi1);
                 break;
-        }
-    ;}
+            case 37:
+                redBoxPosi1-=10;
+                redBox.style.left=redBoxPosi1+"px";
+                // console.log(redBoxPosi1);
+                declareWin(redBoxPosi1, blueBoxPosi1);
+                break;
+            case 65:
+                blueBoxPosi1-=11;
+                blueBox.style.left=blueBoxPosi1+"px";
+                // console.log(redBoxPosi1);
+                declareWin(redBoxPosi1, blueBoxPosi1);
+                break;};}
+
+
+
+    function declareWin(a, b) {
+
+        // var blueBox = document.getElementById('blueBox');
+
+        if (redBoxPosi1 > 500 && blueBoxPosi1 < 500) {
+            console.log('Red Wins!!!');}
+        else if (blueBoxPosi1 > 500 && redBoxPosi1 < 500) {
+            console.log('Blue Wins!!!');}
+        else {console.log("it's a race to the party!!!");};}
+
+
+    // function declareWin(a, b) {
+
+    //     var scoreBoard = document.getElementById('scoreBoard');
+
+    //     if (redBoxPosi1 > 500 && blueBoxPosi1 < 500) {
+    //         // console.log('Red Wins!!!');}
+    //         var status = document.createTextNode('Red Wins!!!');}
+    //         scoreboard.appendChild(status);
+    //     else if (blueBoxPosi1 > 500 && redBoxPosi1 < 500) {
+    //         // console.log('Blue Wins!!!');}
+    //         var status = document.createTextNode('Blue Wins!!!');
+    //         scoreboard.appendChild(status);}
+    //     else if (blueBoxPosi1 > 500 && redBoxPosi1 > 500) {
+    //         // console.log('Blue Wins!!!');}
+    //         var status = status;
+    //         scoreboard.appendChild(status);}
+
+    //     else {console.log("it's a race to the party!!!")
+    //         var status = status;
+    //         scoreboard.appendChild(status);};}
+
+
+// ideas
+    // have declareWin() upate the scoreboard div
+    // exit for loop
+    // players select parrots
+    // reset game and keep track of prior wins
+    // andrew wk 'Welcome to the party friend.  Enjoy a slice of fine pizza and the festivities, winner!'
+    // background and images scale right
+
+    // can go backwards
+
+
 ;}
-
-
-
-
-// sample
-// window.onload = function(){
-//     var redBoxposition=0;
-//     var blueBoxPosi1=0;
-//     document.addEventListener("keydown",moveBox);
-
-//     var player1 = document.getElementById("player1");
-//     var player2 = document.getElementById("player2");
-
-//     function moveBox(e) {
-//         switch(e.keyCode) {
-//             case 90:
-//                 player1position+=10;
-//                 player1.style.left=player1position+"px";
-//                 break;
-//             case 39:
-//                 player2position+=10;
-//                 player2.style.left=player2position+"px";
-//                 break;
-//         }
-//     }
-// }
-
-
-
-
-
-
-
-
-// scratch
-// function movePlayer() {
-//     var key = key just hit
-//     element.addEventListener("keydown", keyDownTextField, true);
-//     var player = if rightArrow then 'red'
-//         if d then blue
-
-// document.getElementById("red").addEventListener("keydown", myFunction);
-
-// function myFunction() { console.log('you hit somethign');}
-//     // document.getElementById("demo").innerHTML = "Hello World";
-// }
